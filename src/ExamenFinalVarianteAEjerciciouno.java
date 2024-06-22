f42f30650a877d7fb0889b949b0814f0a0c1f328
Sebasmiumg <jrossac@miumg.edu.gt>
2024-06-22 10:40:19 -0600

    SegundoCommit

---
+++ src/ExamenFinalVarianteAEjerciciouno.java
@@ -0,0 +1,22 @@
+public class ExamenFinalVarianteAEjerciciouno {
+    public static void Calculadora() {
+    }
+    public void calculadora(int num1, int num2, char operador) {
+        int resultado = 0;
+        switch (operador) {
+            case '+':
+                resultado = num1 + num2;
+                break;
+            case '-':
+                resultado = num1 - num2;
+                break;
+            case '*':
+                resultado = num1 * num2;
+                break;
+            case '/':
+                resultado = num1 / num2;
+                break;
+        }
+        System.out.println("Resultado: " + resultado);
+    }
+}
